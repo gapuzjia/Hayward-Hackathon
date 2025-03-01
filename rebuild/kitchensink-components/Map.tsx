1e1e263780256625329c45554f7e4badac67ffd4
gapuzjia <146483086+gapuzjia@users.noreply.github.com>
2025-03-01 11:54:43 -0800

    made skeleton

---
+++ rebuild/kitchensink-components/Map.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Map = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>this is Map Screen</Text>
+    </View>
+  );
+};
+
+export default Map;
